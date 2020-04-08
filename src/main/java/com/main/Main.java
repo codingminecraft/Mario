ecdaa2d9e1e374917b4ead5f8734ded1af41e03f
Gabe Ambrosio <gamesbygabe1@gmail.com>
2020-04-08 17:23:38 -0500

    Add fatal error handling, with directive to submit bug to Github

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -2,9 +2,29 @@ package com.main;
 
 import com.jade.Window;
 
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        Window window = Window.getWindow();
-        window.run();
+        try {
+            Window window = Window.getWindow();
+            window.run();
+        } catch (Throwable e) {
+            System.out.println("Oh no! It looks like an error has occured. Please copy the contents of error.log to https://github.com/codingminecraft/Mario/issues.");
+
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter("error.log"));
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                e.printStackTrace(pw);
+                writer.write(sw.toString());
+                writer.close();
+            } catch (IOException e2) {
+                System.out.println("A truly fatal error has occured. I cannot open a file to write the original application error. Please log this to the same website as above.");
+                e2.printStackTrace();
+            }
+
+            e.printStackTrace();
+        }
     }
 }
